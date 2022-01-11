3c466df22c6d0f26a520d68cb24312a971b37918
sukantahui <huisukanta77@gmail.com>
2022-01-11 23:06:38 +0530

    added owner

--- institution_angular/src/app/app-routing.module.ts
+++ institution_angular/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'owner', loadChildren: () => import('./pages/owner/owner.module').then(m => m.OwnerModule) },
   { path: 'Sidenav', loadChildren: () => import('./sidenav/sidenav.module').then(m => m.SidenavModule) },
 
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
@@ -16,6 +17,8 @@ const routes: Routes = [
   { path: 'Header', loadChildren: () => import('./header/header.module').then(m => m.HeaderModule) },
 
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+
+  { path: 'Owner', loadChildren: () => import('./pages/owner/owner.module').then(m => m.OwnerModule) },
 
 ];
 
