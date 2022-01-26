5b38ca7fdc70290390d8806fe898214d070bafb2
mounitbhandari <tataibhandari@gmail.com>
2022-01-26 22:41:33 +0530

    add district table

---
+++ institution_api/database/migrations/2022_01_26_223318_create_districts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('state_id')->references('id')->on('states');
+            $table->string('district_name')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
